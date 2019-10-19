cb2fdbee8bc1e03ad661f5fffefe9e991da250f8
Abderrahman Mhadden <amhadd18@student.aau.dk>
2019-10-19 14:34:21 +0200

    Add comment and Fix tabbing.

--- src/server/WordGenerator.java
+++ src/server/WordGenerator.java
@@ -8,43 +8,46 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class WordGenerator{
-    private String[] wordList = new String[969];
-    private String[] wordsToDisplay = new String[25];
+class WordGenerator{
 
+    private String[] wordList = new String[969]; //Array of the words in the text file
+    private String[] wordsToDisplay = new String[25]; //Array of the words to send to the client
 
-    public void readFile(){
+    void readFile(){
         try{
-            String fileName = "src/server/data/wordsList.txt"; //path of the text file with words
-            int wordAmount = 0; // Used to fill the words in the list
+            
+            String fileName = "src/server/data/wordsList.txt"; //Path of the text file with words
+            int wordAmount = 0; //Used to fill the words in the list
+
+            //Read from text file and save in a string
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
+
+            //Fill the array with words
             while(line != null){
                 wordList[wordAmount] = line;
                 wordAmount++;
                 line = bufferedReader.readLine();
             }
+
+            //Create set to avoid word repitition
             Set<String> wordListSet = new HashSet<>();
             Collections.addAll(wordListSet, wordList);
-            int size = wordListSet.size();
-            int item = new Random().nextInt(size);
+
+            int size = wordListSet.size(); //Size of the set
+            int item = new Random().nextInt(size); //Random number from the set's size
             
+            //Create array of words to display from set
             for(int j = 0; j < 25; j++)
             {
-                    wordsToDisplay[j] = wordListSet.toArray(new String[wordListSet.size()])[item];                    
+                wordsToDisplay[j] = wordListSet.toArray(new String[size])[item];                    
                 item = new Random().nextInt(size);      
             }
+            bufferedReader.close();
         }
         catch(IOException e){
                 e.printStackTrace();
         }
-
     }
-
-    public int getRandom(int max){
-        double rand = Math.random()*max;
-        return (int)rand;
-    }
-
 }
